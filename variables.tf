bccd24654361f6b3edc007018e3cd436fae8cf0f
Jolomisan Edema <jolomisan01@gmail.com>
2023-02-05 23:26:43 +0100

    initial commit

---
+++ variables.tf
@@ -0,0 +1,82 @@
+variable "region" {
+  description = "AWS region"
+  type = string
+  default = "us-east-1"
+}
+
+variable "vpc_block" {
+  description = "cidr block and name tag for the vpc"
+  type = object({
+    cidr_block = string
+    name = string
+  })
+}
+
+variable "subnet_blocks" {
+  description = "cidr, availability zone and name tag for the subnets"
+  type = map(object({
+    cidr = string
+    az = string
+  }))
+}
+
+variable "igw" {
+  description = "internet gateway name"
+  type = string
+}
+
+variable "rtb" {
+  description = "cidr block and name tag for route table"
+  type = object({
+    cidr_block = string
+    name = string
+  })
+}
+
+variable "sg" {
+  description = "Application load balancer and instances security group name"
+  type = list(string)
+}
+
+variable "inbound_ports" {
+  description = "Security group inbound ports"
+  type = list(number)
+  default = [80,443]
+}
+
+variable "ssh_key" {
+  description = "SSH Key name"
+  type = string
+}
+
+variable "instance_names" {
+  description = "Instance names"
+  type = list(string)
+}
+
+variable "domain_names" {
+  description = "Describes the domain and subdomain names"
+  type = map(string)
+}
+
+variable "token" {
+  description = "Name.com API token"
+  type = string
+#  default = "4159c6fdc98f4baa025a6f49c038d6590af1fe7d"
+}
+
+variable "username" {
+  description = "Name.com username"
+  type = string
+ # default ="misandotcom-test"
+}
+
+variable "lb_name" {
+  description = "Load balancer name"
+  type = string
+}
+
+variable "tg_name" {
+  description = "Target group name"
+  type = string
+}
